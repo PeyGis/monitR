54b9645af4203ae971ceaa52ef98a65a5b234203
Pages <icoffie@idscorpgh.com>
2018-01-19 21:35:34 +0000

    #splashscreen activity added

---
+++ app/src/main/java/com/capstone/icoffie/monitr/SplashscreenActivity.java
@@ -0,0 +1,26 @@
+package com.capstone.icoffie.monitr;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashscreenActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
